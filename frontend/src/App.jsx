ae0be8843c618125af5e2d1eddbcd190fdaf185b
Joel <joela1o31@gmail.com>
2024-05-05 16:40:51 -0400

    Added add/checkbox/trash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import Todo from "./Todo";
 
 export default function App() {
-  const[todos, setTodos] = useState("");
+  const[todos, setTodos] = useState([]);
+  const[content, setContent] = useState("");
   
   useEffect(() => {
     const getTodos = async() => {
@@ -14,13 +16,45 @@ export default function App() {
     getTodos();
   }, [])
 
+  const createNewTodo = async(e) => {
+    e.preventDefault();
+    if(content.length > 3){
+      const res = await fetch("/api/todos",{
+        method: "POST",
+        body: JSON.stringify({todo: content}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const newTodo = await res.json();
+
+      setContent("");
+      setTodos([...todos, newTodo]);
+    }
+  };
 
 
   return (
     <main className="container">
-      <h1>ToDo App</h1>
-      <div>
-      {(todos.length > 0) && <pre>{JSON.stringify(todos, null, 2)}</pre>}
+      <h1 className="title">ToDo</h1>
+      <form className="form" onSubmit={createNewTodo}>
+        <input 
+          type="text" 
+          value={content} 
+          onChange={(e) => setContent(e.target.value)} 
+          placeholder="Enter a new todo..."
+          className="form__input"
+          required 
+        />
+        <button type="submit">Create Todo</button>
+      </form>
+
+      <div className="todos">
+        {(todos.length > 0) &&
+          todos.map((todo) => (
+            <Todo todo={todo} setTodos={setTodos} key={todo._id} />
+          ))
+        }
       </div>
     </main>
   );

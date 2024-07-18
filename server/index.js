891e80d53db3bb989a8274633495ea337bd46c4e
Joel <joela1o31@gmail.com>
2024-07-18 11:28:41 -0400

    connect db to back end

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
 //import express library
 const express = require('express');
+
+//import connectToMongoDB function from database.js
+const {connectToMongoDB} = require('./database');
+
+//import routes from routes.js
 const router = require('./routes');
 
 //create an express app
@@ -9,9 +15,12 @@ const app = express();
 app.use("/api", router);
 
 //make a port for the server to listen on
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-//listen to server on localhost:5000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const startServer = async() => {
+    await connectToMongoDB();
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+};
+startServer();
